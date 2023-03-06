d3764bb5215957a5494e9c0b5fb1339fceb7bca7
Guma96 <mateusz.kutarba96@gmail.com>
2023-03-06 12:08:22 +0100

    as

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,26 +3,24 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
 const gallery = document.querySelector('.gallery');
 
-const galleryItem = galleryItems
+const galleryElements = galleryItems
   .map(
     item =>
-      `<a class="gallery__item" href="${item.original}"><img src="${item.preview}"  class="gallery__image" alt="${item.description}"</img></a>`
+      `
+    <a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+  `
   )
   .join('');
 
-gallery.innerHTML = galleryItem;
+gallery.insertAdjacentHTML('beforeend', galleryElements);
 
-const showFullscreenImage = event => {
-  event.preventDefault();
-
-  if (event.target.nodeName !== 'IMG') return;
-};
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  caption: true,
+const biggerImg = new SimpleLightbox('.gallery a', {
+  captions: true,
   captionsData: 'alt',
   captionDelay: 250,
 });
-// console.log(galleryItems);
