15ca099afbee3a6a9531b3db22be799f75a32e8b
Guma96 <mateusz.kutarba96@gmail.com>
2023-03-06 11:52:58 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+const _ = require('lodash');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const videoCurrentTime = currentTime => {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(currentTime.seconds)
+  );
+};
+
+player.on('timeupdate', _.throttle(videoCurrentTime, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
